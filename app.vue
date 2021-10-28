c9e673c302793ea46140ee40c358a44e5ffcaa73
Samuel Ebuka <samiebuka@impronta.es>
2021-10-28 06:53:20 +0200

    inicar

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <!-- Remove this component to get started! -->
+    <!-- x hola mundo
+    _ tailwind css
+    - copiar codigo
+    - subirlo a github? -->
+    <div class="w-full h-64 bg-green-200">hola</div>
+    <NuxtWelcome />
+  </div>
+</template>
