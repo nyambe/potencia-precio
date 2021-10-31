87f38c915c30af8f894cd82ace65beedc9883dd6
Samuel Ebuka <samiebuka@impronta.es>
2021-10-31 12:28:55 +0100

    inicio

--- app.vue
+++ app.vue
@@ -1,11 +1,36 @@
 <template>
   <div>
     <!-- Remove this component to get started! -->
-    <!-- x hola mundo
-    _ tailwind css
-    - copiar codigo
-    - subirlo a github? -->
-    <div class="w-full h-64 bg-green-200">hola</div>
-    <NuxtWelcome />
+    <!-- 
+    x hola mundo
+    x tailwind css
+    x copiar codigo
+    - subirlo a github o netlify? -->
+    <ComparadorMacs :items="comparativa" />
   </div>
 </template>
+<script setup>
+import "tailwindcss/tailwind.css";
+
+import ComparadorMacs from "./components/ComparadorMacs.vue";
+
+const saludos = "hola como ests";
+const comparativa = [
+  {
+    mac: "Air",
+    clave: "MACBOOKAIR2020_MAIN",
+  },
+  {
+    mac: '13"',
+    clave: "MBP_MAIN",
+  },
+  {
+    mac: '14"',
+    clave: "MBP2021_14",
+  },
+  {
+    mac: '16"',
+    clave: "MBP2021_16",
+  },
+];
+</script>
